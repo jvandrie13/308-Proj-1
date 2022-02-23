bb3d36962787741e56cebd3e2de05e417a50bfe3
jvandrie <jvandrie@co2061-23.ece.iastate.edu>
2022-02-23 12:33:54 -0600

    continuos input

--- shell.c
+++ shell.c
@@ -7,6 +7,7 @@
 
 
 #define clear() printf("\033[H\033[J")
+#define BUFFER_SIZE BUFSIZ
 
 // Function to print out beginning information
 void initialize()
@@ -24,15 +25,16 @@ void initialize()
 	clear();
 }
 
-//Print Working Directory
+//Print Working Directory (pwd)
 void printDir()
 {
     char cwd[1024];
     getcwd(cwd, sizeof(cwd));
-    printf("\n%s\n", cwd);
+    printf("Working Directory: %s\n", cwd);
 }
 
-//Change Directory
+
+//Change Directory (cd)
 void changeDir(char dir[]){
 	
 	if(dir){
@@ -41,16 +43,61 @@ void changeDir(char dir[]){
 	else{
 		chdir(getenv("HOME"));
 	}
-
-	printf("Changed Directory to:");
-	printDir();
+	char cwd[1024];
+    	getcwd(cwd, sizeof(cwd));
+	printf("Changed Directory to: %s\n", cwd);
 }
+
+//Print Process ID (pid)
+void printPID(){
+	printf("Current Process ID: %d\n", getpid());
+}
+
+//Print Parent Procces ID (ppid)
+void printPPID(){
+	printf("Current Parent Process ID: %d\n", getppid());
+}
+
 
 int main(int argc, char **argv)
 {
-char wd[128];
+	char *p = "308sh> ";
+	char cmd[100];
+	char *cmds[10];
+	int i, k;
+	
+	//check for custom name
+	if (argv[1]){
+		if (strcmp(argv[1], "-p") == 0){
+			p = argv[2];
+		}
+	}
 
-changeDir("..");
-//printDir();
-//initialize();
+		
+	while(1){
+		printf("%s ", p);
+		fgets(cmd, 100, stdin);
+
+		i=0;
+		cmds[i] = strtok(cmd, " \n");
+		while(cmds[i] !=NULL){
+			cmds[++i] = strtok (NULL, " \n");
+		}
+	
+		if (strcmp(cmds[0], "pwd") == 0){
+			printDir();
+		}
+		else if (strcmp(cmds[0], "pid") == 0){
+			printPID();
+		}
+		else if (strcmp(cmds[0], "ppid") == 0){
+			printPPID();
+		}
+		else if (strcmp(cmds[0], "cd") == 0){
+			changeDir(cmds[1]);
+		}
+		else if (strcmp(cmds[0], "exit") == 0){
+			return 0;
+		}
+	}
 }
